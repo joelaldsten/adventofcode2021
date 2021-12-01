34e4a2ed6400a30a06854d0e4744f7626dd2ef81
joelaldsten <aldsten.joel@gmail.com>
2021-12-01 22:44:19 +0100

    Day1

---
+++ day1/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	FILE *fp = fopen("input.txt", "r");
+	int nbrOfInputs = 2000;
+	int nbrOfInc = 0;
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int temp;
+	fscanf(fp, "%d", &temp);
+	a += temp;
+	fscanf(fp, "%d", &temp);
+	a += temp;
+	b += temp;
+	fscanf(fp, "%d", &temp);
+	a += temp;
+	b += temp;
+	c += temp;
+	for(int i = 3; i < nbrOfInputs; ++i) {
+		fscanf(fp, "%d", &temp);
+		switch (i % 3) {
+		case 0 :
+			b += temp;				//last add
+			c += temp;				//second add
+			if (b > a) nbrOfInc++;	//check if inc
+			a = temp;				//first add
+			break;
+		case 1 :
+			a += temp;
+			c += temp;
+			if (c > b) nbrOfInc++;
+			b = temp;
+			break;
+		case 2 :
+			a += temp;
+			b += temp;
+			if (a > c) nbrOfInc++;
+			c = temp;
+			break;
+		}
+	}
+	fclose(fp);
+	printf("number of increases: %d", nbrOfInc);
+	return 0;
+}
