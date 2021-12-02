1f3059fc8d1473f2f5d809e6b44574c58a3d5a2f
joelaldsten <aldsten.joel@gmail.com>
2021-12-02 20:07:14 +0100

    day2

---
+++ day2/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	FILE *fp = fopen("input.txt", "r");
+    int x = 0;
+    int y = 0;
+    int aim = 0;
+    char input[255];          ///varför crashar detta då vi har 9???
+    for (int i = 0; i < 1000; i++) {
+        fgets(input, 12, fp);
+
+        switch (input[0]) {
+        case 'f':
+            x += input[8]-48;
+            y += aim * (input[8]-48);
+            break;
+        case 'd':
+            aim += input[5]-48;
+            break;        
+        case 'u':
+            aim -= input[3]-48;
+            break;            
+        }
+    }
+    printf("x: %d, y: %d, sum %d \n", x, y, x*y);
+	fclose(fp);
+	return 0;
+}
